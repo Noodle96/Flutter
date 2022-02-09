de7f2ab0e832161293a3efaa3a35d2246516dac5
Noodle96 <jorgealfredomarino2@gmail.com>
2022-02-09 00:29:28 -0500

    add flutter project counter

---
+++ contador/lib/src/app.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Center(
+        child: Text('Hello World !'),
+      ),
+    );
+  }
+}
