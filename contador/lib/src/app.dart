fdda260199a717d0985134ff1c932d4f65267b25
Noodle96 <jorgealfredomarino2@gmail.com>
2022-02-09 22:31:00 -0500

    widget statefulWidget and add_minus_zero buttons

--- contador/lib/src/app.dart
+++ contador/lib/src/app.dart
@@ -1,14 +1,17 @@
 // ignore_for_file: use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
-import 'package:contador/src/pages/home_page.dart';
+import 'package:contador/src/pages/contador_page.dart';
+//import 'package:contador/src/pages/home_page.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Center(
-        child: HomePage(),
+        //child: ContadorPage(),
+        child: ContadorPage(),
       ),
     );
   }
---
+++ contador/lib/src/pages/contador_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class ContadorPage extends StatefulWidget {
+  const ContadorPage({Key? key}) : super(key: key);
+
+  @override
+  createState() => _ContadorPageState();
+}
+
+class _ContadorPageState extends State<ContadorPage> {
+  final _estiloTexto = const TextStyle(fontSize: 20);
+  int _contador = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page Tittle with stateful'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Numero de clicks: ', style: _estiloTexto),
+            Text('$_contador', style: _estiloTexto),
+          ],
+        ),
+      ),
+      floatingActionButton: _crearButtons(),
+      floatingActionButtonLocation: FloatingActionButtonLocation
+          .miniCenterFloat, // bloque de los tres botones
+    );
+  }
+
+  Widget _crearButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        FloatingActionButton(
+          child: const Icon(Icons.remove),
+          onPressed: () {
+            _contador--;
+            setState(() {});
+          },
+        ),
+        FloatingActionButton(
+          child: const Icon(Icons.exposure_zero),
+          onPressed: () {
+            _contador = 0;
+            setState(() {});
+          },
+        ),
+        FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            _contador++;
+            setState(() {});
+          },
+        ),
+      ],
+    );
+  }
+}
