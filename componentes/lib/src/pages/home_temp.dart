09dd4858922fdc096f680ff76549a9d7c5de09a9
Noodle96 <jorgealfredomarino2@gmail.com>
2022-02-11 00:49:19 -0500

    Create ListTiles from static list

--- componentes/lib/src/pages/home_temp.dart
+++ componentes/lib/src/pages/home_temp.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
-  const HomePageTemp({Key? key}) : super(key: key);
+  HomePageTemp({Key? key}) : super(key: key);
 
+  final cartasComunes = [
+    'Caballero',
+    'arqueras',
+    'esqueletos',
+    'espiritu de hielo',
+    'espiritu electrico',
+    'zap'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,20 +18,24 @@ class HomePageTemp extends StatelessWidget {
         title: const Text('Componentes Temp'),
       ),
       body: ListView(
-        children: const [
-          ListTile(
-            title: Text("ListTile 1"),
-          ),
-          Divider(),
-          ListTile(
-            title: Text("ListTile 2"),
-          ),
-          Divider(),
-          ListTile(
-            title: Text("ListTile 3"),
-          ),
-        ],
+        children: _crearItems(),
       ),
     );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> listaWidgets = <Widget>[];
+    for (String cart in cartasComunes) {
+      final widgetTemp = ListTile(
+        title: Text(cart),
+      );
+      listaWidgets
+        ..add(widgetTemp)
+        ..add(const Divider(
+          color: Color.fromRGBO(100, 210, 130, 1),
+          thickness: 3,
+        ));
+    }
+    return listaWidgets;
   }
 }
